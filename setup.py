94a62bf109adb994e1abecf4be80d2bb70737f3f
Madhav Sanap <madhavsanap@Madhavs-MacBook-Pro.local>
2026-07-15 01:27:52 +0530

    deployment ready

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="FLIPKART RECOMMENDER",
+    version="0.2",
+    author="jk",
+    packages=find_packages(),
+    install_requires = requirements,
+)
+
+
